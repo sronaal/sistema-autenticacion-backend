def5dbb9963d7d3136827e6e87ed6735c7262de2
Ronal S <ronal.sanx21@gmail.com>
2025-10-02 13:32:36 -0500

    Middleware registrar usuario

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express'
-import { iniciarSesion, registrarUsuario, changePassword, forgetPassword} from '../controllers/authController.js'
-
+import { iniciarSesion, registrarUsuario, changePassword, forgetPassword } from '../controllers/authController.js'
+import { validacionFormRegistrarUsuario, validaciones } from '../middleware/validators/validaciones.js'
 const routes = Router()
 
 
 routes.post('/login', iniciarSesion)
-routes.post('/register', registrarUsuario)
+routes.post('/register', validacionFormRegistrarUsuario, validaciones, registrarUsuario)
 routes.post('/forget', changePassword)
 routes.post('/change-password', forgetPassword)
 
