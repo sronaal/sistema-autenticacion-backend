08b4914d55fcd2e4cf53e4b3bacb056aeb6a224b
Ronal S <ronal.sanx21@gmail.com>
2025-10-02 13:30:40 -0500

    Validaciones Registrar Usuario

---
+++ middleware/validators/validaciones.js
@@ -0,0 +1,37 @@
+import { body, validationResult } from "express-validator";
+
+
+export const validacionFormRegistrarUsuario = [
+
+    body('nombre').notEmpty().withMessage('El nombre es requerido').trim().escape(),
+
+    body('apellido').notEmpty().withMessage('El apellido es requerido')
+    .trim().escape(),
+
+    body('email').isEmail().withMessage('El correo electrónico es inválido')
+    .trim().escape().normalizeEmail(),
+
+    body('password').isLength({min: 3}).withMessage('La contraseña debe tener minimo 3 caracteres')
+    .trim().escape()
+]
+
+
+
+
+
+export const validaciones = (req, res, next) => {
+
+    const errores = validationResult(req)
+
+    if (!errores.isEmpty()){
+        const extraerErrores = errores.array().map(err => ({
+            [err.path]: err.msg
+        }));
+
+        return res.status(422).json({
+            errores: extraerErrores
+        })
+    }
+
+    next();
+}
